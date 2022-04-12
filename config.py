7a192dc36d261886a9bb398a903263a85e10adc2
Erfan <erfan226@gmail.com>
2022-04-12 23:58:14 +0430

    Update parameters

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 BASE_CONFIG = {
-    "model_path": "erfan226/persian-t5-paraphraser",
+    # "model_path": "erfan226/persian-t5-paraphraser",
+    "model_path": "Ahmad/parsT5-base",
     "local_model_path": "/gd2/MyDrive/models/persian-t5-style-paraphraser" # You must change this to your own directory
 }
 TRAIN_CONFIG = {
     "paraphrase_dataset_path": "data/paraphrase_data.txt", # For the transfer task
     "formal_dataset_path": "data/formal_data.txt", # For the classification task
     "informal_dataset_path": "data/informal_data.txt", # For the classification task
-    "dataset_limit": 5000,
+    "dataset_limit": -1, # Use all of available data. Limit it if needed
+    "loss_threshold": 0.001
 }
 GENERATION_CONFIG = {
     "text_similarity": 0.8,
