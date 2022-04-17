6f4476483ea1ed3143d0ba274f7c6b3156953e4b
Erfan <erfan226@gmail.com>
2022-04-17 19:16:24 +0430

    Update model path comment

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BASE_CONFIG = {
     "model_path": "erfan226/persian-t5-paraphraser",
-    "local_model_path": "/gd2/MyDrive/models/persian-t5-style-paraphraser" # You must change this to your own directory
+    "local_model_path": "/gd2/MyDrive/models/persian-t5-style-paraphraser" # Direcotry to save the trained/downloaded (outside of HuggingFace) model. You must change this to your own directory.
 }
 TRAIN_CONFIG = {
     "paraphrase_dataset_path": "data/paraphrase_data.txt", # For the transfer task
